f76c8fe44c08bd55aee158251e58fe520bb7bc49
Trevor C. Justus <127045055+trevorcj@users.noreply.github.com>
2025-01-02 16:02:15 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,18 @@
+# Subway Passenger Counter App
+
+This project is one of the projects in the Scrimba JavaScript course by Per Borgen.
+
+This web application was built with HTML, CSS, and JavaScript.
+I refactored the code by using the addEventListener method on the increment and save buttons.
+
+## Usage
+
+1. Clone the repository
+   ```bash
+   git clone https://github.com/trevorcj/subway-passenger-counter.git
+   ```
+2. Click `index.html` to open it in your browser.
+
+---
+
+Happy Coding! 👩🏾‍💻
---
+++ index.js
@@ -0,0 +1,19 @@
+// Selecting HTML elements
+let saveEl = document.getElementById("save-el");
+let countEl = document.getElementById("count-el");
+let incrementBtn = document.getElementById("increment-btn");
+let saveBtn = document.getElementById("save-btn");
+
+let count = 0;
+
+incrementBtn.addEventListener("click", function () {
+  count += 1;
+  countEl.textContent = count;
+});
+
+saveBtn.addEventListener("click", function () {
+  let countStr = ` ${count} - `;
+  saveEl.textContent += countStr;
+  countEl.textContent = 0;
+  count = 0;
+});
